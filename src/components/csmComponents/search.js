69edb887ce96e9b38a9d563d4da5f4ef2fa0dc41
Ankit Aman <ankit7091aman@gmail.com>
2022-05-15 12:23:12 +0530

    value fetched

--- src/components/csmComponents/search.js
+++ src/components/csmComponents/search.js
@@ -5,7 +5,7 @@ import Card from './displayCards'
 
 
 const temp = [
-  {firstName: 'Jeffry', lastName: 'Osmund', email: 'josmund0@ask.com'},
+  {firstName: 'Jeffry', lastName: 'Osmund', email: 'josmund0@ask.com', position:'manager'},
   {firstName: 'Julian', lastName: 'Conkey', email: 'jconkey1@mayoclinic.com'},
   {firstName: 'Goldi', lastName: 'Hayers', email: 'ghayers2@elegantthemes.com'},
   {firstName: 'Lurline', lastName: 'Petraitis', email: 'lpetraitis3@last.fm'},
@@ -17,24 +17,33 @@ const temp = [
   {firstName: 'Quintus', lastName: 'Swalough', email: 'qswalough9@csmonitor.com'},
   ]
 
-  //var displayArr = [];
+  var displayArr = [];
 
 
 const Search = () => {
       
     //  temp.forEach((e) => console.log(e))
       
-      const options = temp.map((e)=>({
+      const options = temp.map((e)=> {
+       // console.log(e.firstName);
+        return {
        
-        value: Object.values(e).join(','), label: <Card firstName={e.firstName} lastName={e.lastName} email={e.email} />
-      }))
+          value: Object.values(e).join(','), label: <Card firstName={e.firstName} lastName={e.lastName} email={e.email} />
+        };
+      })
       
 
-      const [disp, setDisp] = useState([])
-
+     
+      
       const handler = e =>{
-        setDisp(e.value);
-        console.log(e.firstName +' ' + e.value?.firstName);
+         displayArr = [];
+        //setDisp(e);
+        console.log('e value')
+        e.map((ele)=> {
+          
+          displayArr.push(ele.value)
+          console.log(displayArr);
+        })
        } 
       
        
@@ -47,10 +56,9 @@ const Search = () => {
             options={options}
             isMulti
             placeholder = {'Add by Name or email'}
-            onChange = {handler}
+            onChange= {handler}
         /> 
     </div>
-    <h1> {disp} </h1>
   </>
   )
 }
