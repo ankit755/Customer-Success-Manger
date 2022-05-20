e195499cd02f72045e513f0022e9391d90703c2a
Ankit Aman <ankit7091aman@gmail.com>
2022-05-20 22:53:05 +0530

    delete fuctionality added

--- src/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-import CsmReducers from "./CsmReducers"
-
-import {combineReducers} from "redux"
-
-const rootReducer = combineReducers ({
-    CsmReducers
-})
-
-export default rootReducer;
--- src/components/csmComponents/DisplayList.js
+++ src/components/csmComponents/DisplayList.js
@@ -1,50 +1,38 @@
 import React, { useState, useEffect } from 'react'
-import {useSelector, useDispatch } from "react-redux"
-import { addTodo, deleteListItem} from '../../Actions/index'  
-
-
-
 import '../CSS/DisplayList.css'
-
-// Template to display the data getting 
-//from onchange & then with button click
-
 const DisplayList = (prop) => {
+  const [data, setData] = useState(new Set())
 
-  let list = [];
- 
-  const dispatch = useDispatch(); 
+  useEffect(() => {
+    if (prop.displayArr) {
+      prop.displayArr.map((e) => setData((oldValue) => new Set([...oldValue, e.value])))
+    }
+  }, [prop])
 
-
-  if (prop.displayArr)
-    // empty array here to prevent from duplicates
-    prop.displayArr.map((ele) => {
-      list.push(ele.value)
-    })
-
+  const deleteItem = (id) => setData(new Set(Array.from(data).filter((_, i) => i !== id)))
 
   return (
 
     <div>
 
       {
-        list.length > 0 && list.map((e, i) => {
+        data.size > 0 && Array.from(data).map((e, i) => {
           const [firstName, lastName, position] = e.split(',');
           return (<div key={i}>
 
             <div className='list' >
-              <div className = 'list_logo'> <h5>{firstName[0]} {lastName[0]}</h5> </div>
+              <div className='list_logo'> <h5>{firstName[0]} {lastName[0]}</h5> </div>
               <div className='list_text'>
-                <h4 >{firstName} {lastName}</h4>
+                <h4>{firstName} {lastName}</h4>
                 <p> {position} </p>
-
 
               </div>
               <div className='trash'>
-                <i className="fa-solid fa-trash fa-lg" onClick={() => dispatch(deleteListItem(e.i))}></i>
+                <i className="fa-solid fa-trash fa-lg" onClick={() => deleteItem(i)} ></i>
               </div>
             </div>
           </div>)
+
         })
       }
     </div>
